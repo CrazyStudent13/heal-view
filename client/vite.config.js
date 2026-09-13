93b1194af796934514bd612737071350a53956de
杨扬 <1076535690@qq.com>
2026-09-13 15:40:18 +0800

    build: enable Vue auto imports

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: ['vue'],
+      dts: './auto-imports.d.ts',
+      vueTemplate: true
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
